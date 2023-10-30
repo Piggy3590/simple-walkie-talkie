25e48ce473a669d9a4f52c1deb527ee03b42597c
Piggy <70088001+Piggy3590@users.noreply.github.com>
2023-10-30 19:45:56 +0900

    Update WalkieTalkieFabric.java

--- fabric/src/main/java/fr/flaton/walkietalkie/fabric/WalkieTalkieFabric.java
+++ fabric/src/main/java/fr/flaton/walkietalkie/fabric/WalkieTalkieFabric.java
@@ -5,12 +5,29 @@ import fr.flaton.walkietalkie.config.ModConfig;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
+import fr.flaton.walkietalkie.block.ModBlocks;
+import fr.flaton.walkietalkie.block.entity.ModBlockEntities;
+import fr.flaton.walkietalkie.item.ModItemGroup;
+import fr.flaton.walkietalkie.item.ModItems;
+import fr.flaton.walkietalkie.item.WalkieTalkieItem;
+import fr.flaton.walkietalkie.network.ModMessages;
+import fr.flaton.walkietalkie.screen.ModScreenHandlers;
+import fr.flaton.walkietalkie.sound.ModSounds;
+
 public class WalkieTalkieFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         ModConfig config = new ModConfig(FabricLoader.getInstance().getConfigDir());
         config.loadModConfig();
 
-        WalkieTalkie.init();
+        ModBlocks.register();
+		ModItems.register();
+		ModItemGroup.register();
+		ModSounds.registerSounds();
+		
+		ModBlockEntities.register();
+		ModScreenHandlers.register();
+
+		ModMessages.registerC2SPackets();
     }
 }
